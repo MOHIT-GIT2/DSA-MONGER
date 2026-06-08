f57608e2f409c3cdd52aec3f5503f0e361703a3a
Singal <2461882@cognizant.com>
2026-06-08 20:11:00 +0530

    Custom Linked List

---
+++ dsa/src/LinkedList/customLL/LL.java
@@ -0,0 +1,47 @@
+package LinkedList.customLL;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public void insertFirstPos(int value){
+        Node node=new Node(value);
+        node.next=head;
+        head=node;
+
+        if (tail==null)
+            tail=head;
+        size++;
+    }
+
+    public void display(){
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.value+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("end");
+    }
+
+    public LL() {
+        this.size=0;
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ dsa/src/LinkedList/customLL/Main.java
@@ -0,0 +1,7 @@
+package LinkedList.customLL;
+
+public class Main {
+    static void main() {
+        LL list=new LL();
+    }
+}
